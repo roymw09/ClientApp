02f97238d3011f41b13ba35ad1eb3cda6c152afc
roywilliams <roymw09@gmail.com>
2022-07-08 14:17:03 -0230

    Removed unused imports

--- app/src/main/java/com/rmw/clientapp/feature/component/CreateMessageIcon.kt
+++ app/src/main/java/com/rmw/clientapp/feature/component/CreateMessageIcon.kt
@@ -9,10 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.rmw.clientapp.ui.theme.ClientAppTheme
 
 @Composable
 fun CreateMessageIcon(
